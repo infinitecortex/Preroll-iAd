27cda71b095d2c774ab98562629f398ed455b764
E. Spencer Johnson <spencer@infiniteCortex.com>
2015-12-18 10:33:13 -0500

    Initial Commit

---
+++ Preroll-iAd/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Preroll-iAd
+//
+
+//  Copyright (c) 2015 Spencer Johnson. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ Preroll-iAd/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Preroll-iAd
+//
+
+//  Copyright (c) 2015 Spencer Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
